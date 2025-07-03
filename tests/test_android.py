a8106cc954aac99660170ac6573b98e16d16b10f
yavv951 <vadimv95189@gmail.com>
2025-07-03 23:34:31 +0300

    add test

--- tests/test_android.py
+++ tests/test_android.py
@@ -7,7 +7,7 @@ def test_android_via_grid():
       "appium:automationName": "UiAutomator2",
       "appium:deviceName": "Android Emulator",
       "platformVersion": "11.0",
-      "appium:app": "/apps/app_debug.apk"
+      "appium:app": "/apps/ru.oneme.app.apk"
     }
 
     driver = webdriver.Remote("http://localhost:4444/wd/hub", caps)
