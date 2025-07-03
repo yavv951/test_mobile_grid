ab3103682bfc9041141339db756f4894c6831f7e
yavv951 <vadimv95189@gmail.com>
2025-07-04 00:36:43 +0300

    add test

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e
 
-# Извлечение APK из XAPK (если есть)
+echo "📦 Extract XAPK if exists..."
 if [ -f /apps/ru.oneme.app.xapk ]; then
-  echo "📦 Extracting XAPK..."
   mkdir -p /apps/extracted
   unzip -o /apps/ru.oneme.app.xapk -d /apps/extracted
   cp /apps/extracted/ru.oneme.app.apk /apps/
 fi
 
-echo "🚀 Starting Appium with base-path /wd/hub..."
+# Ждём готовности зависимостей
+echo "⏳ Waiting for Grid Hub (http://selenium-hub:4444)..."
+sleep 3
+/wait --host selenium-hub --port 4444 --timeout 60
+
+echo "⏳ Waiting for Appium internal readiness (http://localhost:4723/wd/hub/status)..."
+/wait --timeout 60 --host localhost --port 4723 --path /wd/hub/status
+
+echo "🚀 Launching Appium..."
 exec appium --config /config/appium1.yml --base-path /wd/hub
