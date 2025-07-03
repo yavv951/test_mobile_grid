4439056066a1c4519b28a733d4ca5219f752f66d
yavv951 <vadimv95189@gmail.com>
2025-07-03 23:51:38 +0300

    add test

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            screenshot = item.name + ".png"
+            driver.save_screenshot(screenshot)
