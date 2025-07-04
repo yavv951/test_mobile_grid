6b337279e60562ca2b11c92c80870302ff0935da
yavv951 <vadimv95189@gmail.com>
2025-07-05 01:19:23 +0300

    add test

--- docker/appium/entrypoint.sh
+++ docker/appium/entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "▶ Запуск Appium с конфигом $1..."
-appium --config "$1" --base-path /wd/hub &
-
-sleep 10
-
-if ! curl -s http://localhost:4723/wd/hub/status | grep -q '"ready":true'; then
-  echo "❌ Appium не готов!"
-  ps aux | grep appium
-  exit 1
-fi
-
-# Не завершать контейнер
-tail -f /dev/null
+echo "Starting Appium Grid with UiAutomator2 driver..."
+appium server \
+  --use-drivers=uiautomator2 \
+  --base-path /wd/hub \
+  --allow-insecure chromedriver_autodownload
