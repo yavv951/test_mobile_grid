6afea75f9386094e8b58e321c47c82aa3791c552
yavv951 <vadimv95189@gmail.com>
2025-07-05 00:47:36 +0300

    add test

--- docker/appium/entrypoint.sh
+++ docker/appium/entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "📦 Extract XAPK if exists..."
-if [ -f /apps/ru.oneme.app.xapk ]; then
-  mkdir -p /apps/extracted
-  unzip -o /apps/ru.oneme.app.xapk -d /apps/extracted
-  cp /apps/extracted/ru.oneme.app.apk /apps/
+echo "▶ Запуск Appium с конфигом $1..."
+appium --config "$1" --base-path /wd/hub &
+
+sleep 10
+
+if ! curl -s http://localhost:4723/wd/hub/status | grep -q '"ready":true'; then
+  echo "❌ Appium не готов!"
+  ps aux | grep appium
+  exit 1
 fi
 
-# Ждём готовности зависимостей
-echo "⏳ Waiting for Grid Hub (http://selenium-hub:4444)..."
-sleep 3
-/wait --host selenium-hub --port 4444 --timeout 60
-
-echo "⏳ Waiting for Appium internal readiness (http://localhost:4723/wd/hub/status)..."
-/wait --timeout 60 --host localhost --port 4723 --path /wd/hub/status
-
-echo "🚀 Launching Appium..."
-exec appium --config /config/appium1.yml --base-path /wd/hub
+# Не завершать контейнер
+tail -f /dev/null
